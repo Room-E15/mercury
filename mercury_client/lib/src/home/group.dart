2d159e3b6c84bd18431fffd3ddad222c6dc7e0e1
Aidan Sacco <saccoa54@gmail.com>
2024-10-20 09:22:52 +0200

    add dyanmic group display

---
+++ mercury_client/lib/src/home/group.dart
@@ -0,0 +1,19 @@
+class Group {
+  const Group(this.id, this.name, this.memberCount, this.responseCount, this.unsafe);
+
+  final int id;
+  final String name;
+  final int memberCount;
+  final int responseCount;
+  final int unsafe;
+  // final List<Response> responses = List<Response>.empty();
+}
+
+class Response {
+  const Response(this.safe, this.battery, this.latitude, this.longitude);
+
+  final bool safe;
+  final int battery;
+  final double latitude;
+  final double longitude;
+}
--- mercury_client/lib/src/home/home_view.dart
+++ mercury_client/lib/src/home/home_view.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'group.dart';
 
 class HomeView extends StatelessWidget {
+  const HomeView({
+    super.key,
+    this.groups = const [
+      Group(1, "Cal Poly Software", 36, 12, 0), 
+      Group(2, "Cal Poly Architecture", 36, 24, 0), 
+      Group(3, "U Chicago", 36, 36, 0), 
+      Group(3, "That Group", 36, 12, 1)],
+  });
 
   static const routeName = '/';
+
+  final List<Group> groups;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +28,7 @@ class HomeView extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               decoration: InputDecoration(
-                hintText: 'Search...',
+                hintText: 'Search Groups',
                 prefixIcon: const Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12.0),
@@ -27,32 +38,82 @@ class HomeView extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: 1, // Number of blank cards
-              itemBuilder: (context, index) {
-                return const Card(
+              restorationId: 'groupList',
+              itemCount: groups.length, // Number of blank cards
+              itemBuilder: (BuildContext context, int index) {
+                final group = groups[index];
+                final progress = group.responseCount / group.memberCount;
+                final anyUnsafe = group.unsafe > 0;
+                return Card(
                   clipBehavior: Clip.antiAliasWithSaveLayer,
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: SizedBox(
-                    height: 100.0, // Height of each blank card
-                    child: Column(children: [
-                      Stack(
-                        children: [
-                          LinearProgressIndicator(
-                            value: 0.5,
-                            backgroundColor: Color.fromARGB(255, 126, 126, 126),
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                            minHeight: 20.0,
-                          ),
-                          Text(
-                            'Card Title',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: InkWell(
+                    onTap: (){},
+                    child: SizedBox(
+                      height: 100.0, // Height of each blank card
+                      child: Column(children: [
+                        Stack(
+                          children: [
+                            LinearProgressIndicator(
+                              value: anyUnsafe ? 1 : progress,
+                              backgroundColor: const Color.fromARGB(255, 126, 126, 126),
+                              valueColor: anyUnsafe
+                                ? const AlwaysStoppedAnimation<Color>(Colors.red)
+                                : const AlwaysStoppedAnimation<Color>(Colors.green),
+                              minHeight: 20.0,
                             ),
-                          ),
+                            Row(children: [
+                              Icon(
+                                anyUnsafe
+                                  ? Icons.error
+                                  : Icons.check,
+                                color: const Color.fromARGB(128, 0, 0, 0),
+                              ),
+                              Text(anyUnsafe
+                                ? group.unsafe > 1 
+                                  ? "${group.unsafe} members are not safe!"
+                                  : "${group.unsafe} member is not safe!"
+                                : "${group.responseCount} of ${group.memberCount} Members Safe",
+                                style: const TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w900,
+                                  color: Color.fromARGB(128, 0, 0, 0)
+                                ),
+                              ),
+                            ],)
+                        ],),
+                        Row(
+                          children: [
+                            Text(
+                              group.name,
+                              style: const TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Icons.chevron_right
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(child:
+                              ElevatedButton(
+                                onPressed: (){},
+                                child: const Text(
+                                    "Send Alert"
+                                  )
+                              )
+                            )
+                        ],)
                       ],)
-                    ],)
-                  ),
+                    ),
+                  )
                 );
               },
             ),
@@ -64,7 +125,7 @@ class HomeView extends StatelessWidget {
 }
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: HomeView(),
   ));
 }
