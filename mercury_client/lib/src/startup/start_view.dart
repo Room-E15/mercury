43881b786bc189f110a574d11019098785ee4a61
Caden Upson <cadenupson@gmail.com>
2024-10-27 18:55:08 +0100

    Input validation added to main page, unable to verify due to flutter

--- mercury_client/lib/src/startup/start_view.dart
+++ mercury_client/lib/src/startup/start_view.dart
@@ -6,11 +6,18 @@ class StartView extends StatelessWidget {
 
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController areaCodeController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
 
   final Widget logo;
 
   StartView({super.key, required this.logo});
+
+  final formKey = GlobalKey<FormState>();
+
+  final String? selectedValue = "+1"; // Current selected value
+
+  final List<String> options = ['+1', '+2', '+3']; // Options list
 
   @override
   Widget build(BuildContext context) {
@@ -23,48 +30,114 @@ class StartView extends StatelessWidget {
           const Padding(
               padding: EdgeInsets.all(40),
               child: Text("Welcome! Please enter your information.")),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: firstNameController,
-              decoration: const InputDecoration(
-                labelText: 'First Name',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: lastNameController,
-              decoration: const InputDecoration(
-                labelText: 'Last Name',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: phoneNumberController,
-              decoration: const InputDecoration(
-                labelText: 'Phone Number',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => VerificationView(
-                    logo: logo,
-                    firstName: firstNameController.text,
-                    lastName: lastNameController.text,
-                    phoneNumber: phoneNumberController.text,
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: firstNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'First Name',
+                    ),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please enter some text';
+                      } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                        return 'Please only use onlyalphabetical characters';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-              ),
-              child: const Text('Submit'),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: lastNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Last Name',
+                    ),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please enter some text';
+                      } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                        return 'Please only use only alphabetical characters';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: DropdownButtonFormField<String>(
+                          value: "selectedValue", // Currently selected value
+                          items: options.map((String option) {
+                            return DropdownMenuItem<String>(
+                              value: option,
+                              child: Text(option),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            // setState(() {
+                            //   selectedValue = newValue;
+                            // });
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Area Code',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          controller: phoneNumberController,
+                          decoration: const InputDecoration(
+                            labelText: 'Phone Number',
+                          ),
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please enter some text';
+                            } else if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                              return 'Please only use only numerical characters';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => VerificationView(
+                                logo: logo,
+                                firstName: firstNameController.text,
+                                lastName: lastNameController.text,
+                                phoneNumber: phoneNumberController.text,
+                              ),
+                            ));
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
