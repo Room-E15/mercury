cf7ce6bcc68b0736d3ceb27473a2b0e4c209507d
Caden Upson <cadenupson@gmail.com>
2024-11-05 08:01:03 +0100

    Initialize db connection

---
+++ backend/src/main/java/com/mercury/demo/entities/Member.java
@@ -0,0 +1,45 @@
+package com.mercury.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String firstName;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String lastName;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String areaCode;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String phoneNumber;
+
+    public Member(final String firstName, final String lastName, final String areaCode, final String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.areaCode = areaCode;
+        this.phoneNumber = phoneNumber;
+    }
+}
