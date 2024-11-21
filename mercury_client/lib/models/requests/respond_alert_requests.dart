cd7d24389a11dd6a9355033f8ed6a9134852ce50
Cameron Wolff <36940948+camwolff02@users.noreply.github.com>
2024-11-21 13:03:37 +0100

    responses are now saved, consider refactoring database

--- mercury_client/lib/models/requests/respond_alert_requests.dart
+++ mercury_client/lib/models/requests/respond_alert_requests.dart
@@ -12,24 +12,25 @@ class RespondAlertRequests extends ServerRequests {
 
   static Future<bool> saveAlertResponse({required String memberId, required bool isSafe}) async {
     final location = await locationService.getCurrentLocation();
-    final batteryPercentage = await batteryService.getBatteryPercentage();
+    final batteryPercent = await batteryService.getBatteryPercentage();
 
     // TODO should we await this, and if the server doesn't respond we don't remove the icon?
-    var response = await post(
+    final response = await post(
       Uri.parse('${ServerRequests.baseURL}$subURL/save'),
       body: {
         'memberId': memberId,
         'isSafe': isSafe.toString(),
         'latitude': location?.latitude.toString() ?? '',
         'longitude': location?.longitude.toString() ?? '',
-        'batteryPercentage': batteryPercentage.toString(),
+        'batteryPercent': batteryPercent.toString(),
       },
     ).onError((error, stackTrace) {
       return Response('', 500);
     });
 
-    log("[$subURL] Alert response sent");
-    return response.statusCode == 200;
+    final success = response.statusCode == 200;
+    log("[$subURL] Alert response sent ${success ? 'successfully' : 'unsuccessfully'}");
+    return success;
   }
 
 }
